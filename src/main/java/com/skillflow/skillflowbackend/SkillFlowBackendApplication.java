2aa2b6fcf3c6c0ce9aa15019f78cd4dbbe6c6605
mohsenfennira <mohsen.fennira@esprit.tn>
2024-11-23 11:01:58 +0100

    First commit + Add Database Config

---
+++ src/main/java/com/skillflow/skillflowbackend/SkillFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.skillflow.skillflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillFlowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillFlowBackendApplication.class, args);
+    }
+
+}
